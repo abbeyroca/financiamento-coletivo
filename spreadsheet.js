25d4aad2d1dddbc8fa8de329ec331e0bf9443b82
Luisa Burini <luisa.burini@gmail.com>
2024-01-04 14:23:18 -0300

    :construction: Testes atualizar barra de progresso

--- spreadsheet.js
+++ spreadsheet.js
@@ -3,64 +3,99 @@ const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
 const sheetName = 'Financiamento - Legaliza o Abbey! (respostas)';
 const query = encodeURIComponent("Select B, E, F, G, H, I, J, K")
 const url = `${base}&sheet=${sheetName}&tq=${query}`
-// Precos por coluna
-const prices = [0, 20, 50, 80, 300, 200, 400, 500]
 
 const data = []
 document.addEventListener('DOMContentLoaded', init)
 const output = document.querySelector('.output')
 function init() {
   console.log("INIT!!!")
-    fetch(url)
-        .then(res => res.text())
-        .then(rep => {
+  fetch(url)
+  .then(res => res.text())
+  .then(rep => {
             //Apaga textos adicionais e extrai so o JSON:
             const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
             const colz = [];
             const tr = document.createElement('tr');
             //Extrai nome das colunas
             jsonData.table.cols.forEach((heading) => {
-                let column = heading.label;
+              let column = heading.label;
                 colz.push(column);
                   // column = "valeu galera!"
                   const th = document.createElement('th');
                   // th.innerText = column;
                   tr.appendChild(th);
-        
-            })
+                  
+                })
             // output.appendChild(tr);
             //Extrai dados das linhas
             jsonData.table.rows.forEach((rowData) => {
                 const row = {};
                 colz.forEach((ele, ind) => {
                     row[ele] = (rowData.c[ind] != null) ? rowData.c[ind].v : '';
-                })
+                  })
                 data.push(row);
+              })
+              processRows(data);
             })
-            processRows(data);
-        })
 }
+
+// Precos por coluna
+const prices = [0, 20, 50, 80, 300, 200, 400, 500]
 
 function processRows(json) {
   var total = 0.0
-  var progressBar = document.getElementById("myBar");
-    json.forEach((row) => {
-        const tr = document.createElement('tr');
+  json.forEach((row) => {
+    const tr = document.createElement('tr');
         const keys = Object.keys(row);
 
         keys.forEach((key) => {
-            const td = document.createElement('td');
-            console.log("Key " + key + "Row " + row[key])
-            td.textContent = row[key];
-            tr.appendChild(td);
+          console.log("Key " + key + " Row " + row[key])
+          if (key.includes("Brinde 1")) {
+            total = total + row[key] * 20.0
+          }
+          if (key.includes("Brinde 2")) {
+            total = total + row[key] * 50.0
+          }
+          if (key.includes("Brinde 3")) {
+            total += 80.0
+          }
+          if (key.includes("CLIENTE VIP")) {
+            total += 300.0
+          }
+          if (key.includes("PATROCINADOR PRATA")) {
+            total += 200.0
+          }
+          if (key.includes("PATROCINADOR OURO")) {
+            total += 400
+          }
+          if (key.includes("DIREITO DE FESTA")) {
+            total += 500
+          }
+          // const td = document.createElement('td');
+          // td.textContent = row[key];
+          // tr.appendChild(td);
         })
         // output.appendChild(tr);
-    })
-}
-
-var i = 0;
-function move() {
-  if (i == 0) {
+      })
+      var myBar = document.getElementById("myBar");
+      var progressBar = document.getElementById("myProgress");
+      console.log("progress bar width " + progressBar.style.width)
+      var textGoal = document.getElementById("textGoal")
+      if (total < 5000) {
+        console.log("Ainda nao alcancou a meta " + total)
+        myBar.style.width = progressBar.style.width * total / 5000;
+        missingAmount = 5000-total
+        textGoal.text = "Faltam " + missingAmount + " reais para alcançar a meta!"
+      } else {
+        myBar.style.width = progressBar.style.width
+        textGoal.text = "Meta alcançada! VALEU GALERA!"
+      }
+      myBar.text = 100*total/5000 + "%"
+    }
+    
+    var i = 0;
+    function move() {
+      if (i == 0) {
     i = 1;
     var elem = document.getElementById("myBar");
     var width = 1;
